d280bc446167714a214b9346c17c98c0f4bb3e31
lucic71 <lucian.popescu187@gmail.com>
2022-03-20 20:11:47 +0200

    Modified fixaout.py script to work with python3
    Maybe it would be a good idea to rewrite the script in bash or sh
    as they change the syntax less often than python.

--- tools/fixaout.py
+++ tools/fixaout.py
@@ -15,17 +15,17 @@ def unwords(ws) :
     return struct.pack('<%dH' % l, *ws)
 
 def read(fn) :
-    f = file(fn, 'rb')
+    f = open(fn, 'rb')
     d = f.read()
     f.close()
     return d
 
 def write(fn, d) :
-    f = file(fn, 'wb')
+    f = open(fn, 'wb')
     f.write(d)
     f.close()
 
 d1 = words(read('a.out'))
 hdr = d1[:8]
-d = [0405, 12+hdr[1], 0, 0, hdr[4], 0] + d1[8:]
+d = [0o405, 12+hdr[1], 0, 0, hdr[4], 0] + d1[8:]
 write("a.out", unwords(d))
